7dfc5c596e74af45d9aa95810b48458f526c9a6e
Priyanshu <priyanshusingh1877@gmail.com>
2023-02-10 18:56:26 +0530

    commit0

--- src/Home.js
+++ src/Home.js
@@ -1,49 +1,49 @@
-import React,{useState,useEffect} from 'react'
-import Card from './Card';
-import { useNavigate } from 'react-router-dom';
-import axios from "axios"
-import css from"./style.css"
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import css from "./style.css";
 
 function Home() {
-  const[data,setData]=useState([]);
+  const [data, setData] = useState(null);
   useEffect(() => {
     getItems();
-  }, [])
+  }, []);
 
-  const getItems=async()=>{
-    try{
-    let result = await axios.get("https://notes2-yvan.onrender.com/getnote");
-    setData(result.data);
-    console.log(data);
-    }
-    catch(e){
+  const getItems = async () => {
+    try {
+      let result = await axios.get("https://notes2-yvan.onrender.com/getnote");
+      console.log(result);
+      setData(result.data);
+      console.log(data);
+    } catch (e) {
       console.log(e);
     }
-  }
+  };
 
   const navigate = useNavigate();
-  const changepage = ()=>{
-    navigate("/addnote")
-  }
+  const changepage = () => {
+    navigate("/addnote");
+  };
 
   return (
     <div className="App">
-      <div className='headline'>
-      <p id="paraline">Notes</p>
-      <p id="paraline1" onClick={changepage}>+ Add Note</p>
+      <div className="headline">
+        <p id="paraline">Notes</p>
+        <p id="paraline1" onClick={changepage}>
+          + Add Note
+        </p>
       </div>
-      <div className='cardDiv'>
-      {
-          data.map((data)=>{
-            console.log("map",data.price);
-           return (<Card data={data}/>)
-           
-          })
-
-      }
-      </div>
+      {data && (
+        <div className="cardDiv">
+          {data.map((item) => {
+            console.log("map", item.price);
+            return <Card data={item} />;
+          })}
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
